c8333092bab1d2b1794c417f3ad27dc4fa152ace
Manuel <milton.sanchezrios88@gmail.com>
2024-05-12 18:46:38 -0500

    componente presupuesto, creado - formulario

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <script setup>
+import { ref } from 'vue';
+import Presupuesto from './components/Presupuesto.vue';
 
+const presupuesto = ref(0)
 </script>
 
 <template>
   <div class="container mx-auto">
     <h1 class="text-4xl text-center font-bold text-indigo-700 mt-8">Planificador de Gastos</h1>
-
+    <div>
+      <Presupuesto
+        v-model:presupuesto="presupuesto"
+      />
+    </div>
   </div>
 </template>
 
---
+++ src/components/Presupuesto.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+<template>
+    <div>
+        <h2>formulario</h2>
+    </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+
+</style>
