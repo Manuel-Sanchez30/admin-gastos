2fadfce78449fc724f9a63b75141cd130a8ef246
Manuel <milton.sanchezrios88@gmail.com>
2024-05-12 18:12:05 -0500

    proyecto iniciado

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container mx-auto">
+    <h1 class="text-4xl text-center font-bold text-indigo-700 mt-8">Planificador de Gastos</h1>
+
+  </div>
+</template>
+
