6854295a02674f58e3ba681d639f37ea0ffa2a0e
Manuel <milton.sanchezrios88@gmail.com>
2024-05-12 20:49:29 -0500

    validacion formulario y se crea componente alerta

--- src/components/Presupuesto.vue
+++ src/components/Presupuesto.vue
@@ -1,14 +1,50 @@
 <script setup>
+import { ref } from 'vue';
+import Alerta from './Alerta.vue'
+
+
+const presupuesto = ref(0)
+const error = ref('')
+
+//defineEmits(['definir-presupuesto'])
+const definirPresupuesto = ()=>{
+    if(presupuesto.value === 0){
+        error.value = 'presupuesto no valido!!!';
+
+        setTimeout(()=>{
+            error.value = ''
+        },3000)
+    }
+
+    return
+}
+
 
 </script>
 <template>
-    <div>
-        <h2>formulario</h2>
+    <div class="flex justify-center mt-4">
+        <form 
+            class="flex flex-col items-center gap-1 mt-4"
+            @submit.prevent="definirPresupuesto"
+        
+        >
+            <Alerta v-if="error">
+                {{ error }}
+            </Alerta>
+            <label for="presupuesto" class="font-semibold">Define el Presupuesto: </label>
+            <input 
+                id="presupuesto"
+                type="number"
+                placeholder="define el presupuesto"
+                min="0"                
+                v-model.number="presupuesto"
+                class="border-none rounded-md text-gray-950 text-center font-medium"    
+            >
+            <input 
+                type="submit"
+                value="Definir Presupuesto"
+                class="bg-green-600 mt-4 w-full rounded-md font-semibold cursor-pointer hover:bg-green-700 transition-colors"    
+            >
+        </form>
     </div>
 </template>
-
-
-
-<style lang="scss" scoped>
-
-</style>
